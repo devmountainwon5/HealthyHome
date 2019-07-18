f19bca58ce5c0b70b9b826964a68232e05a7b889
Jacob Mosteller <jacobmosteller722@gmail.com>
2019-07-17 19:55:47 -0600

    wireframe for the home,health,todos

---
+++ healthy-home/src/Views/Home/Subviews/HomeHealth/HomeHealth.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class HomeHealth extends Component {
+    render() {
+        return (
+            <div>
+                Healthy home
+            </div>
+        )
+    }
+}
+
+export default HomeHealth;
---
+++ healthy-home/src/Views/Home/Subviews/ToDos/ToDos.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class ToDos extends Component {
+    render() {
+        return (
+            <div>
+                To dos
+            </div>
+        )
+    }
+}
+
+export default ToDos;
