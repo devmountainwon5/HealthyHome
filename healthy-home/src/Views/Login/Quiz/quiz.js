14839f25a7d5c63285aa7445e39a5d9e255bab9e
Jared <jaredbolduc@yahoo.com>
2019-07-20 12:10:43 -0600

    saving

---
+++ healthy-home/src/Views/Login/Quiz/quiz.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import './quiz.css';
+
+
+class Quiz extends Component {
+
+    componentDidMount(){
+		axios.get('/api/quiz/questions')
+		.then(({data})=>{
+			if (data.success) {
+				this.props.setCart(data.cartItems);
+			} else if (!data.isLoggedIn) {
+				this.props.history.push('/');
+			} else {
+				alert('something blew up');
+			}
+		})
+    }
+
+    render() {
+		const quizItems = this.props.quizItems.map((e)=>{
+			return <QuizItem key={e.id} removeFromCart={this.removeFromCart} id={e.id} image_url={e.image_url} price={e.price} name={e.name} />
+		})
+		return (
+			<div className="quiz">
+				<Header />
+				{quizItems}
+			</div>
+		);
+	}
+}
+
+export default connect(state => state, Actions)(Quiz) ;
