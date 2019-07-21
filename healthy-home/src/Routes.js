f5ca82d3ede3f3526cd70d7b9224b3b41e859d50
Brittany Hancock <bhancock@utah.gov>
2019-07-21 12:22:34 -0600

    added some login frontend functionality

--- healthy-home/src/Routes.js
+++ healthy-home/src/Routes.js
@@ -6,6 +6,7 @@ import React from 'react';
 import Home from './Views/Home/Home';
 import Calendar from './Views/Calendar/Calendar';
 import Login from './Views/Login/Login/login';
+import Quiz from './Views/Login/Quiz/quiz';
 // import Tips from './Views/Tips/Tips';
 
 export default (
@@ -15,5 +16,6 @@ export default (
         <Route path="/calendar" component={Calendar} />
         <Route path="/tips" render={()=><div>Tips</div>} />
         <Route path="/" component={Login} />
+        <Route path="/register" component={Quiz}/>
     </Switch>
 );
