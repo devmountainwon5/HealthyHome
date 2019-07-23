de94ff9bc586adb592ed66a1115006274ffc6b44
Abby <malsr.abby@gmail.com>
2019-07-23 16:53:16 -0600

    navbar and proxy

--- healthy-home/src/Routes.js
+++ healthy-home/src/Routes.js
@@ -7,17 +7,17 @@ import Home from './Views/Home/Home';
 import Calendar from './Views/Calendar/Calendar';
 import Login from './Views/Login/Login/login';
 import Register from './Views/Login/Register/register';
-import NavBar from './Views/Home/NavBar/NavBar';
+// import NavBar from './Views/Home/NavBar/NavBar';
 // import Tips from './Views/Tips/Tips';
 
 export default (
     <Switch>
-        <NavBar />
+        {/* <NavBar /> */}
         {/* Replace with proper component={} things later */}
         <Route path="/home" component={Home} />
         <Route path="/calendar" component={Calendar} />
         <Route path="/tips" render={()=><div>Tips</div>} />
-        <Route path="/" component={Login} />
         <Route path="/register" component={Register}/>
+        <Route exact path="/" component={Login} />
     </Switch>
 );
