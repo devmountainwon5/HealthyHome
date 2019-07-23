24e26883281a7bdee478c108a1b235a5e2bb699d
Brent Whitehead <bheadwhite@gmail.com>
2019-07-22 20:27:16 -0600

    added login

--- healthy-home/src/Views/Login/Login/login.js
+++ healthy-home/src/Views/Login/Login/login.js
@@ -7,24 +7,24 @@ import Register from "../Register/register"
 let color = "blue"
 export default function Login(props) {
 	const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
-    const [title, setTitle] = useState("Login")
+	const [password, setPassword] = useState("")
+	const [title, setTitle] = useState("Login")
 
-    useEffect(()=> {
-        window.addEventListener('resize', doTheColorThing)
-        return () => {
-            window.removeEventListener('resize', doTheColorThing)
-        }
-    }, [])
+	useEffect(() => {
+		window.addEventListener("resize", doTheColorThing)
+		return () => {
+			window.removeEventListener("resize", doTheColorThing)
+		}
+	}, [])
 
-    useEffect(()=> {
-        document.title = title
-    },[title])
+	useEffect(() => {
+		document.title = title
+	}, [title])
 
 	return (
 		<div>
 			<div className='login-box'>
-                <input type='text' value={title} name='title' onChange={e=> setTitle(e.target.value)} />
+				<input type='text' value={title} name='title' onChange={e => setTitle(e.target.value)} />
 				<Link to='/home'>go home</Link>
 				<hr />
 				Login
@@ -39,7 +39,7 @@ export default function Login(props) {
 					value={password}
 					onChange={e => setPassword(e.target.value)}
 				/>
-				<button type='submit' onClick={logUserIn}>
+				<button type='submit' onClick={()=>logUserIn(email, password)}>
 					Login
 				</button>
 				<Link to='/register'>Register</Link>
@@ -48,15 +48,26 @@ export default function Login(props) {
 	)
 }
 
-function doTheColorThing(){
-    if(color == 'blue'){
-        color = 'red'
-    } else {
-        color = 'blue'
-    }
-    document.getElementById('root').style.background = color
+function doTheColorThing() {
+	if (color == "blue") {
+		color = "red"
+	} else {
+		color = "blue"
+	}
+	document.getElementById("root").style.background = color
 }
 
-function logUserIn() {
-	console.log("lets login")
+function logUserIn(email, password) {
+	const loginUser = {
+		email,
+		password
+	}
+	axios.post("/auth/login", loginUser).then(({ data }) => {
+		if (data.success) {
+			// props.setUser(data.user)
+			// props.history.push("/home")
+		} else {
+			alert("Username or password did not match our records")
+		}
+	})
 }
