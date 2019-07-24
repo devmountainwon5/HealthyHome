6bac828355c28d3b66678a43acbe9127b97f950b
KrisCoop <47994388+KrisCoop@users.noreply.github.com>
2019-07-23 20:06:23 -0600

    fixed NavBar, put dummy data in UpcomingEvents, changed routes '/home' to go to HomeHealth component

--- healthy-home/src/App.js
+++ healthy-home/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { HashRouter as Router } from 'react-router-dom';
 import './App.css';
+import NavBar from './Views/Home/NavBar/NavBar'
 
 import routes from './Routes';
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className="healthy-home">
       <Router>
+        <NavBar />
         {routes}
       </Router>
     </div>
--- healthy-home/src/Routes.js
+++ healthy-home/src/Routes.js
@@ -7,6 +7,7 @@ import Home from './Views/Home/Home';
 import Calendar from './Views/Calendar/Calendar';
 import Login from './Views/Login/Login/login';
 import Register from './Views/Login/Register/register';
+import HomeHealth from './Views/Home/Subviews/HomeHealth/HomeHealth';
 // import NavBar from './Views/Home/NavBar/NavBar';
 // import Tips from './Views/Tips/Tips';
 
@@ -14,7 +15,7 @@ export default (
     <Switch>
         {/* <NavBar /> */}
         {/* Replace with proper component={} things later */}
-        <Route path="/home" component={Home} />
+        <Route path="/home" component={HomeHealth} />
         <Route path="/calendar" component={Calendar} />
         <Route path="/tips" render={()=><div>Tips</div>} />
         <Route path="/register" component={Register}/>
