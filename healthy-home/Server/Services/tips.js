fc5099ed63f6e2775a4b17f3007db9f3ab29c58e
Abby <malsr.abby@gmail.com>
2019-07-27 08:55:53 -0600

    adjusting tips endpoint

--- healthy-home/Server/Services/tips.js
+++ healthy-home/Server/Services/tips.js
@@ -2,7 +2,7 @@ module.exports = {
     retrieveAll: (req, res, next) => {
         const db = req.app.get('db');
 
-        db.home_tips_table.find()
+       return db.home_tips_table.find()
         .then((tips) => {
             return {
                     success: true, 
