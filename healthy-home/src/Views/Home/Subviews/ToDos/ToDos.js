1d0380ed223f25deac98ec7055c884bd3cfaecb5
Jacob Mosteller <jacobmosteller722@gmail.com>
2019-07-29 14:50:30 -0600

    changed to hooks and a functional component

--- healthy-home/src/Views/Home/Subviews/ToDos/ToDos.js
+++ healthy-home/src/Views/Home/Subviews/ToDos/ToDos.js
@@ -1,30 +1,37 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import NavBar from './../../NavBar/NavBar';
 
-class ToDos extends Component {
-    constructor(props){
-        super(props)
+export default function Todos() {
+    const [homeTips, setHomeTips] = useState("Home Tips will display here")
+    const [yardTips, setYardTips] = useState("Yard Tips will display here")
 
-        this.state={
-            homeTips:'display home tips here',
-            yardTips:'display yard tips here'
-        }
-    }
-    render() {
-        return (
-            <div>
-                <NavBar/>
-                <div className='tipsBox'>
-                    <h2>Tips for the Home</h2>
-                    {this.state.homeTips}
-                </div>
-                <div className='tipsBox'>
-                    <h2>Tips for the Yard</h2>
-                    {this.state.yardTips}
-                </div>
+    return (
+        <div>
+            <NavBar/>
+            <div className='tipsBox'>
+                <h2>Tips for the Home</h2>
+                {homeTips}
             </div>
-        )
-    }
+            <div className='tipsBox'>
+                <h2>Tips for the Yard</h2>
+                {yardTips}
+            </div>
+        </div>
+    )
 }
 
-export default ToDos;
+// class ToDos extends Component {
+//     constructor(props){
+//         super(props)
+
+//         this.state={
+//             homeTips:'display home tips here',
+//             yardTips:'display yard tips here'
+//         }
+//     }
+//     render() {
+       
+//     }
+// }
+
+// export default ToDos;
