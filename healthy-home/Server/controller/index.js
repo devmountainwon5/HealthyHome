ac317743776f4111173cbf1f573f5895380d40c2
Jared <jaredbolduc@yahoo.com>
2019-07-29 21:24:37 -0600

    major update endpoints

--- healthy-home/Server/controller/index.js
+++ healthy-home/Server/controller/index.js
@@ -1,8 +1,10 @@
 const user = require('./user');
 const tips = require('./tips');
+const questions = require('./questions');
 
 
 module.exports = (app) => {
     app.use('/auth', user);
     app.use('/tips', tips);
+    app.use('/questions', questions);
 }
