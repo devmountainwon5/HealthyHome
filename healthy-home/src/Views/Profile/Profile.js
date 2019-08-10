5ad149cd8d55bf52a937c20c408f3c95c5475bac
Brittany Hancock <bhancock@utah.gov>
2019-08-10 13:25:40 -0600

    tasks backend stuff

---
+++ healthy-home/src/Views/Profile/Profile.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import NavBar from 'Views/Home/NavBar/NavBar';
+import axios from 'axios';
+
+export default class Profile extends Component {
+    render() {
+        return (
+            
+            <div>
+                <NavBar/>
+                <div>
+                    Information
+                </div>
+                <div>
+                    User Options
+                    Username:
+                    Email:
+                    Profile Picture:
+                </div>
+                <div>
+                    Contact
+                </div>
+            </div>
+        )
+    }
+}
