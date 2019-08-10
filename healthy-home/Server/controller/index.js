e736f840a0c725a133f46291e4b89ae97c902589
StyeEye <48033738+StyeEye@users.noreply.github.com>
2019-08-10 10:55:21 -0600

    Auth middleware setup (#122)
    * Connected basic authentication middleware
    
    It is not currently used on any /auth/ endpoint
    
    * Fixed typos related to authentication middleware

--- healthy-home/Server/controller/index.js
+++ healthy-home/Server/controller/index.js
@@ -4,13 +4,14 @@ const questions = require('./questions');
 const barometer = require('./barometer')
 const emails = require('./emails');
 const todo = require('./todo');
+const { authMiddleware } = require('../Middleware/auth');
 
 
 module.exports = (app) => {
     app.use('/auth', user);
-    app.use('/tips', tips);
-    app.use('/emails', emails); 
-    app.use('/barometer', barometer); 
-    app.use('/questions', questions);
-    app.use('/todo', todo);
+    app.use('/tips', authMiddleware, tips);
+    app.use('/emails', authMiddleware, emails);
+    app.use('/barometer', authMiddleware, barometer);
+    app.use('/questions', authMiddleware, questions);
+    app.use('/todo', authMiddleware, todo);
 }
