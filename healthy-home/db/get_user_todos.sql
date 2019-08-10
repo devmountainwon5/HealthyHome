1ad53395de30932c760499a603a540f1451b6d28
Brent Whitehead <bheadwhite@gmail.com>
2019-08-10 14:08:38 -0600

    updated dev

--- healthy-home/db/get_user_todos.sql
+++ healthy-home/db/get_user_todos.sql
@@ -1,4 +1,4 @@
-SELECT u.user_id as user, cd.completed_date_id as id, ut.users_todos_id as todo_ID, list.todo_id as real_todo_id, list.todo_item, cd.completed_date as last_completed_date, list.frequency_id, list.active
+SELECT u.user_id as userID, cd.completed_date_id as id, ut.users_todos_id as todo_ID, list.todo_id as real_todo_id, list.todo_item, cd.completed_date as last_completed_date, list.frequency_id, list.active
 FROM user_todos_table ut 
 JOIN users u ON u.user_id = ut.user_id
 JOIN completed_date_table cd ON cd.users_todos_id = ut.users_todos_id
--- healthy-home/src/Views/Home/Subviews/HomeHealth/HouseGraphic/HouseGraphic.js
+++ healthy-home/src/Views/Home/Subviews/HomeHealth/HouseGraphic/HouseGraphic.js
@@ -1,28 +1,27 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import {connect} from 'react-redux';
-import * as actions from '../../../../../Ducks/action_creator';
+import * as actions from 'Ducks/action_creator';
 
 import './HouseGraphic.css';
 
-import niceHouse from '../../../../../Assets/HealthyHouse.png';
-import midHouse from '../../../../../Assets/MidHouse.png';
-import grossHouse from '../../../../../Assets/GrossHouse.png';
+import niceHouse from 'Assets/HealthyHouse.png';
+import midHouse from 'Assets/MidHouse.png';
+import grossHouse from 'Assets/GrossHouse.png';
 
 
 function HouseGraphic(props){
     const [pic, setPic] = useState()
     const [loading, setLoading] = useState(false)
-    console.log(niceHouse)
     useEffect(() => {
         getHouseGraphic();
-    }, [getHouseGraphic]);
+    }, []);
 
     function getHouseGraphic() {
         const {user_id} = props.user
         axios.post("/barometer/retrieveScore", {user_id}).then(({data}) => {
-            setLoading(true)
             if(typeof data === 'number'){
+                setLoading(true)
                 if(data*100 >= 85){
                     setPic(niceHouse)
                 } else if (data*100 <85 && data*100 > 45){
