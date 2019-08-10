6bdf9f80816e5bd8701e9b8829eb667b50061e92
StyeEye <ColonialBuff@protonmail.com>
2019-08-10 10:38:11 -0600

    Connected basic authentication middleware
    It is not currently used on any /auth/ endpoint

--- healthy-home/Server/controller/index.js
+++ healthy-home/Server/controller/index.js
@@ -4,13 +4,14 @@ const questions = require('./questions');
 const barometer = require('./barometer')
 const emails = require('./emails');
 const todo = require('./todo');
+const { authMiddleWare } = require('../Middleware/auth');
 
 
 module.exports = (app) => {
     app.use('/auth', user);
-    app.use('/tips', tips);
-    app.use('/emails', emails); 
-    app.use('/barometer', barometer); 
-    app.use('/questions', questions);
-    app.use('/todo', todo);
+    app.use('/tips', authMiddleWare, tips);
+    app.use('/emails', authMiddleWare, emails);
+    app.use('/barometer', authMiddleWare, barometer);
+    app.use('/questions', authMiddleWare, questions);
+    app.use('/todo', authMiddleWare, todo);
 }
