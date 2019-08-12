4c62526bbbd47a561246bbd97feb1aa31705dab9
KrisCoop <47994388+KrisCoop@users.noreply.github.com>
2019-08-12 17:07:37 -0600

    Authentication works now

--- healthy-home/src/Views/Profile/Profile.js
+++ healthy-home/src/Views/Profile/Profile.js
@@ -1,8 +1,21 @@
-import React from "react"
+import React, {useEffect} from "react"
 import NavBar from "Views/NavBar/NavBar"
 import { connect } from "react-redux"
+import axios from 'axios';
 
 function Profile(props) {
+
+	useEffect(() => {
+		// Auth: Is there a current user session?
+		axios.get('/auth/me')
+			.then((response) => {
+				console.log(response.data.success)
+				if(!response.data.success){
+					props.history.push('/')
+				}
+			})
+	})
+
 	const { first_name: firstname, last_name: lastname, email, phone_num: phone } = props.user
 	const name = firstname + " " + lastname
 
