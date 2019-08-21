8d255445570ee927f8fccaa2a4c9a75819456579
Jacob Mosteller <jacobmosteller722@gmail.com>
2019-08-21 17:55:46 -0600

    working

--- db/change_user_data.sql
+++ db/change_user_data.sql
@@ -1,3 +1,3 @@
 update users
-set first_name = ${fname}, last_name = ${lname}, email = ${email}, phone_num = ${num}
+set first_name = ${fname}, last_name = ${lname}, phone_num = ${num}
 where user_id = ${id}
--- src/Views/Profile/Profile.js
+++ src/Views/Profile/Profile.js
@@ -4,64 +4,79 @@ import { connect } from "react-redux"
 import axios from "axios"
 import * as Action from "../../Ducks/action_creator"
 
-function Profile(props) {
-    const [edit, setedit] = useState(false)
-    const { first_name: firstname, last_name: lastname, email, phone_num: phone, user_id: id } = props.user
-    const { address_line_1, city, state, zip } = props.address
-    const [fName, setfName] = useState(firstname)
-    const [lName, setlName] = useState(lastname)
-    const [nEmail, setnEmail] = useState(email)
-    const [num, setnum] = useState(phone)
-    const name = fName + " " + lName
-    const [F, setF] = useState('')
-    const [L, setL] = useState('')
-    const [N, setN] = useState('')
+function Profile({
+	user: { first_name: firstname, last_name: lastname, email, phone_num: phone, user_id: id },
+	address: { address_line_1, city, state, zip },
+	setAddress
+}) {
+	const [edit, setedit] = useState(false)
+	const [fName, setfName] = useState(firstname)
+	const [lName, setlName] = useState(lastname)
+	const [num, setnum] = useState(phone)
+	const reset = () => {
+		setfName(firstname)
+		setlName(lastname)
+		setnum(phone)
+	}
+	const updateInfo = (first, last, number) => {
+		axios.post("/change", { first, last, number })
+	}
 
-    useEffect(() => {
-        axios.post("/auth/info", { user_id: id }).then(({ data }) => {
-            console.log(data)
-            props.setAddress(data)
-        })
-    }
-        , [])
+	useEffect(() => {
+		axios.post("/auth/info", { user_id: id }).then(({ data }) => {
+			console.log(data)
+			setAddress(data)
+		})
+	}, [])
 
-    return (
-        <div>{!edit && (
-            <>
-                <NavBar activeComponent='Profile' />
-                <div>
-                    <p>User Information First name: {name}</p>
-                    <p>Email: {nEmail}</p>
-                    <p>Phone Number: {num}</p>
-                    <p>Home Address: {address_line_1}</p>
-                    <p>Zip Code : {zip}</p>
-                    <p>City: {city}</p>
-                    <p>State: {state}</p>
-                    <button onClick={() => setedit(true)}>edit</button>
-                </div>
-                <div>contact us</div>
-                {console.log(name)}
-                {console.log(nEmail)}
-                {console.log(num)}
-            </>
-        )}
-            {edit && (
-                <>
-                    <NavBar activeComponent='Profile' />
-                    First Name:<input type="text" value={fName} onChange={e => setF(e.target.value) } />
-                    Last Name:<input type="text" value={lName} onChange={e => setL(e.target.value) } />
-                    Phone Number:<input type="text" onChange={e =>  setN(e.target.value) } /> 
-                    <button onClick={() => {
-                        setedit(false)
-                        setfName(F)
-                        setlName(L)
-                        setnum(N)
-                    }}>Submit</button>
-                    <button onClick={() => setedit(false)}>Cancel</button>
-                </>
-            )}
-        </div>
-    )
+	useEffect(() => {
+		reset()
+	}, [edit])
+
+	return (
+		<div>
+			{!edit && (
+				<>
+					<NavBar activeComponent='Profile' />
+					<div>
+						<p>User Information</p>
+						<p>First name: {firstname}</p>
+						<p>Last name: {lastname}</p>
+						<p>Email: {email}</p>
+						<p>Phone Number: {phone}</p>
+						<p>Home Address: {address_line_1}</p>
+						<p>Zip Code : {zip}</p>
+						<p>City: {city}</p>
+						<p>State: {state}</p>
+						<button onClick={() => setedit(true)}>edit</button>
+					</div>
+					<div>contact us</div>
+				</>
+			)}
+			{edit && (
+				<>
+					<NavBar activeComponent='Profile' />
+					First Name:
+					<input type='text' value={fName} onChange={e => setfName(e.target.value)} />
+					Last Name:
+					<input type='text' value={lName} onChange={e => setlName(e.target.value)} />
+					Phone Number:
+					<input type='text' value={num} onChange={e => setnum(e.target.value)} />
+					<button
+						onClick={() => {
+							setedit(false)
+							updateInfo(fName, lName, num)
+						}}>
+						Submit
+					</button>
+					<button onClick={() => setedit(false)}>Cancel</button>
+				</>
+			)}
+		</div>
+	)
 }
 
-export default connect(state => state, Action)(Profile)
+export default connect(
+	state => state,
+	Action
+)(Profile)
