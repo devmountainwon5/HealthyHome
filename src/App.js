0c815b120c227d191aab948839a3eef5257715ff
Brittany Hancock <47994252+birdnmax@users.noreply.github.com>
2019-08-21 19:57:15 -0600

    snackbar functionality for login page (#200)

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ function App() {
   return (
     <div className="healthy-home">
       <Router>
-        {/* <NavBar /> */}
         {routes}
       </Router>
     </div>
--- src/Views/Login/login.js
+++ src/Views/Login/login.js
@@ -2,12 +2,16 @@ import React, { useState } from "react"
 import { Link } from "react-router-dom"
 import { connect } from "react-redux"
 import * as Actions from "../../Ducks/action_creator"
+import Snackbar from "../Snackbar/Snackbar"
 import "./login.css"
 import httpRequest from "../../shared/services/http_request";
 
 function Login(props) {
 	const [email, setEmail] = useState("")
 	const [password, setPassword] = useState("")
+	const [isErr, setIsErr] = useState(false)
+	const errMsg = 'Credentials did not match'
+
 	const logUserIn = (email, password) => {
 		const loginUser = {
 			email,
@@ -19,8 +23,8 @@ function Login(props) {
 				props.setUser(data.user);
 				props.history.push("/homehealth");
 			})
-			.catch(err => {
-				console.log(err);
+			.catch(() => {
+				setIsErr(true)
 			})
 	}
 
@@ -32,25 +36,28 @@ function Login(props) {
 	}
 
 	return (
-		<div className='login-box'>
-			<h1 className='login-title'>Healthy Homes</h1>
-			<form className='login-values' onSubmit={onSubmit}>
-				<input type='text' placeholder='Email' name='setEmail' value={email} onChange={e => setEmail(e.target.value)} />
-				<input
-					type='password'
-					placeholder='Password'
-					name='setPassword'
-					value={password}
-					onChange={e => setPassword(e.target.value)}
-				/>
-				<button type='submit' onClick={() => logUserIn(email, password)}>
-					Login
-				</button>
-				<Link className='link' to='/register'>
-					Register
-				</Link>
-			</form>
-		</div>
+		<>
+			<div className='login-box'>
+				<h1 className='login-title'>Healthy Homes</h1>
+				<form className='login-values' onSubmit={onSubmit}>
+					<input type='text' placeholder='Email' name='setEmail' value={email} onChange={e => setEmail(e.target.value)} />
+					<input
+						type='password'
+						placeholder='Password'
+						name='setPassword'
+						value={password}
+						onChange={e => setPassword(e.target.value)}
+					/>
+					<button type='submit' onClick={() => logUserIn(email, password)}>
+						Login
+					</button>
+					<Link className='link' to='/register'>
+						Register
+					</Link>
+				</form>
+			</div>
+			<Snackbar message={errMsg} isErr={isErr} />
+		</>
 	)
 }
 
