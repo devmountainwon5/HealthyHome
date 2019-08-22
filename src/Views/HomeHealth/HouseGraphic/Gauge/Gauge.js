663a383dad4047f6774b7bab311ee99a0f10f54f
Abby S <47788095+als735@users.noreply.github.com>
2019-08-21 20:38:11 -0600

    Home page colors (#202)
    * first commit
    
    * new main page updates

--- src/Views/HomeHealth/HouseGraphic/Gauge/Gauge.js
+++ src/Views/HomeHealth/HouseGraphic/Gauge/Gauge.js
@@ -7,7 +7,7 @@ function Gauge(props){
 
     return(
         <div className="GaugeMain">
-            <GaugeChart id="gauge-chart3" nrOfLevels={20} colors={["#FF5F6D", "rgb(135, 255, 23)"]} arcWidth={0.3} percent={percent}/>
+            <GaugeChart id="gauge-chart3" nrOfLevels={20} colors={["#FF5F6D", "rgb(135, 255, 23)"]} arcWidth={0.30} marginInPercent={0.10} percent={percent}/>
         </div>
     )
 
