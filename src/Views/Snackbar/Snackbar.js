03c341ad8692c77361700c177ce05fbfeb5fb014
Brittany Hancock <47994252+birdnmax@users.noreply.github.com>
2019-08-23 15:54:36 -0600

    Brit snacks (#203)
    * snackbar functionality for login page
    
    * working on the snackbar functionality
    
    * resolved snackbar conflicts
    
    * resolved snackbar conflicts
    
    * Snackbar can display more than once
    
    * universal snackbar working for login, register, and todos

--- src/Views/Snackbar/Snackbar.js
+++ src/Views/Snackbar/Snackbar.js
@@ -1,28 +1,17 @@
-import React, { useState, useEffect } from 'react';
-import Styles from './Snackbar.module.css';
+import React, { useEffect, useCallback, useRef } from "react"
+import Styles from "./Snackbar.module.css"
 
-export default function Snackbar({isErr, message}){
-    const [isActive, setIsActive] = useState(false)
-
-
+export default function Snackbar({ isActive, setIsActive, message }) {
+    let timerRef = useRef()
+    const setTimer = useCallback(() => {
+        const timer = setTimeout(() => {
+            setIsActive(false)
+        }, 3000)
+        timerRef.current = timer
+    }, [setIsActive])
     useEffect(() => {
-        console.log('useEffect')
-        activateSnackbar()
-    }, [isErr])
-
-    const activateSnackbar = () => {
-        if(isErr && !isActive){
-            setTimeout(() => {
-                setIsActive(false);
-            }, 3000);
-            setIsActive(true)
-        }
-    }
-    console.log(isActive)
-        return (
-            
-            <div className = {isActive ? [Styles.snackbar, Styles.open].join(" ") : Styles.snackbar} id="snackbar">
-                {message}
-            </div>
-    )
+        setTimer()
+        return () => clearTimeout(timerRef.current)
+    }, [setTimer])
+    return <div className={isActive ? [Styles.snackbar, Styles.open].join(" ") : Styles.snackbar}>{message}</div>
 }
